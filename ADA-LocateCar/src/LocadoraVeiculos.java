f477d9a86563dc73fe17a0ad9d2f5afb6d0f6963
mariannyneris2004 <mariannyneris2004@gmail.com>
2023-09-12 09:04:29 -0300

    Correção

--- ADA-LocateCar/src/LocadoraVeiculos.java
+++ ADA-LocateCar/src/LocadoraVeiculos.java
@@ -127,37 +127,4 @@ public class LocadoraVeiculos {
             System.out.println(e.getMessage());
         }
     }
-
-    private static double calcularValorAluguel(int horasAlugadas, String tipoVeiculo, Cliente cliente) {
-        double valorDiaria;
-        switch (tipoVeiculo.toUpperCase()) {
-            case "PEQUENO":
-                valorDiaria = 100.00;
-                break;
-            case "MEDIO":
-                valorDiaria = 150.00;
-                break;
-            case "SUV":
-                valorDiaria = 200.00;
-                break;
-            default:
-                valorDiaria = 0.00;
-        }
-    
-        int dias = horasAlugadas / 24;
-        int horasRestantes = horasAlugadas % 24;
-        if (horasRestantes > 0) {
-            dias++;
-        }
-    
-        double valorTotal = dias * valorDiaria;
-    
-        if (cliente.getDocumento().length() == 11 && dias > 5) {
-            valorTotal *= 0.95; // Desconto de 5% para cliente Pessoa Física com mais de 5 diárias
-        } else if (cliente.getDocumento().length() == 14 && dias > 3) {
-            valorTotal *= 0.90; // Desconto de 10% para cliente Pessoa Jurídica com mais de 3 diárias
-        }
-    
-        return valorTotal;
-    }
 }
